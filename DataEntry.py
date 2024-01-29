037b904c09fd70a8f2b3f75ffe71ca7012db708d
Vi-Gn <gnwarem9@gmail.com>
2024-01-29 01:31:13 +0100

    try to encapsulate data entry and meds and find a path in btw

--- DataEntry.py
+++ DataEntry.py
@@ -3,13 +3,18 @@ from ItemMeds import *
 
 
 class DataEntry:
-    def __init__(self, Path):
+    def __init__(self, Path: str):
         self.Path = Path
         self.FileRef = File(Path)
         self.DataArray = []
 
     def __del__(self):        
         self.FileRef.Close()
-        
-    def FindId(self, Medicament):
-        
+    
+    def Open(self, Mode: str):
+        self.FileRef.Open(Mode)
+
+    def FindId(self, Id: int):
+        self.Open("r")
+        for i in range()
+        self.FileRef.ReadLine()
