07deab22a32b5d00f9dd82729f0cdc6d6b2d6322
Vi-Gn <gnwarem9@gmail.com>
2024-01-30 02:31:19 +0100

    can write by adding items and read json

--- JS.py
+++ JS.py
@@ -11,46 +11,33 @@ class DataManager:
     def __del__(self):
         self.File.close()
 
-    def GetData(self) -> dict:
+    def GetData(self, KeyName: str = "Meds") -> dict:
+        DataDict = { KeyName: []}
         self.File = open(self.Path, "r")
-        print(len(self.File.read()))
+        fileLength = len(self.File.read())
         self.File.seek(0)
-        DataDict = json.load(self.File)
+        if ( fileLength != 0):
+            DataDict = json.load(self.File)
         self.File.close()
         return DataDict
 
-    def GetDataArray(self, KeyName: str = "Meds") -> list:
-        Currentdata = self.GetData()
-        DataArray = list(Currentdata[KeyName])
-        return DataArray
+    # def GetDataArray(self, KeyName: str = "Meds") -> list:
+    #     Currentdata = self.GetData()
+    #     DataArray = list(Currentdata[KeyName])
+    #     return DataArray
     
     def AddItemToFile(self, MedName: str, MedPrice: float, MedQuantity: int,  KeyName: str = "Meds"):
         Currentdata = self.GetData()
-        CurrentdataArray = Currentdata[KeyName]
-        count = len(CurrentdataArray)
+        count = len(Currentdata[KeyName])
         if count == 0:
-            Id = 0
+            Id = 1
         else:
-            Id = count
-        
+            Id = count + 1
         Currentdata[KeyName].append({ "id": Id, "name": MedName, "Price": MedPrice, "Quantity": MedQuantity })
-        print(Currentdata[KeyName])
-        # self.File = open(self.Path, "w")
-        # json.dump(Currentdata, self.File, indent=4)
-        # self.File.close()
-        # NewData = json.load(file)
-        # tempData = tempDataFile["Meds"]
-        # tempData.append({ "id": 4, "name": "doli", "Price": 18, "Quantity": 50 })
-        # json.dump(Data,file,indent=4)
-        # file.close()
+        self.File = open(self.Path, "w")
+        json.dump(Currentdata, self.File, indent=4)
+        self.File.close()
+        
 
-
-    def LoadItemsFromFile(Path: str):
-        file = open(Path, "r")
-        data = json.load(file)
-        medData = data["Meds"]
-        medData.append({ "id": 4, "name": "doli", "Price": 18, "Quantity": 50 })
-        # medData[]
-        for med in medData:
-            print(med)
-        file.close()
+    def RemoveItemByName(self, MedName: str,  KeyName: str = "Meds"):
+        
