9c539b975491d37a6515971e720a74b42a5f6643
Sphiyo <55040297+Sphiyo@users.noreply.github.com>
2024-02-18 11:37:04 +0100

    insertion good with file manager and open file

--- Workdir/App.py
+++ Workdir/App.py
@@ -31,31 +31,52 @@ def NewDir():
     ap.app.title(ap.title + "         " + ap.workdir)
     ClearFileTable()
     ap.InitDir()
-    print(ap.workdir, '-----------------')
+    # print(ap.workdir, '-----------------')
 
 def NewFile():
     ap.file = filedialog.askopenfilename(initialdir=ap.workdir)
     ClearTable()
-    print(ap.file)
+    # print(ap.file)
 
 
-def OpenFile():
+def OpenFile(path: str = ''):
     # tempApp = Toplevel(app)
-    ap.file = filedialog.askopenfilename(initialdir=ap.workdir)
-    print(ap.file)
+    if path == '':
+        ap.file = filedialog.askopenfilename(initialdir=ap.workdir)
+    else:
+        ap.file = path
+    # print(ap.file)
     ClearTable()
 
     DataRef = ReadCSV(path=ap.file)
-    print(DataRef)
+    # print(DataRef)
     data1 = []
     for ls in DataRef:
         data1.append(list(ls.values()))
-        print(list(ls.values()))
+        # print(list(ls.values()))
     length = len(list(data1)[0])
     for contact in list(data1):
         ap.treeTable.insert('', END, values=contact)
 
+def OpenDirLoaded():
+    # path = filedialog.askdirectory(initialdir=ap.workdir)
+    path = os.path.abspath('..\\')
+    node_id = ap.treeFile.insert('', "end", text=ap.workdirName, values= path ,open=False)
+    OpenDir(node_id)
 
+def OpenDir(node = '', path = os.path.abspath('..\\Workdir')):
+    # Iterate over directories in the current path
+    for dir_name in os.listdir(path):
+        # print(dir_name)
+        dir_path = os.path.join(path, dir_name)
+        value = dir_path.replace('\\', '/')
+        
+        # Add directory to the Treeview
+        node_id = ap.treeFile.insert(node, "end", text=dir_name, values= value ,open=False)
+        # If the current item is a directory, recursively populate its subdirectories
+        if os.path.isdir(dir_path):
+            OpenDir(node_id, dir_path)
+  
 
 
 class TableApplication:
@@ -65,6 +86,8 @@ class TableApplication:
         self.title = title
         self.winWidth = resX
         self.winHeight = resY
+        self.app.state("zoomed")
+        # self.app.attributes("-fullscreen", True)
         self.resolution = f"{resX}x{resY}"
         self.app.title(self.title)
         self.app.iconbitmap(iconPath)
@@ -73,12 +96,16 @@ class TableApplication:
         # self.app.maxsize(width= 895, height= 450)
         self.workdirName = 'Workdir'
         self.workdir = os.path.abspath(f"../{self.workdirName}")
+        self.fileName = 'Data/data.csv'
+        self.filepath = os.path.abspath(f"{self.fileName}")
+        # print(self.filepath)
         self.AddFileMenu()
         self.app.bind("<Configure>", self.on_window_resize)
         self.mainFrame = LabelFrame(self.app, text="App")
         self.mainFrame.grid_rowconfigure(0, weight=1)
         self.mainFrame.grid_columnconfigure(1, weight=1)
-        self.mainFrame.pack(fill="both", padx=5, pady=5)
+        self.mainFrame.pack(fill="both", padx=5, pady=5, expand=1)
+        
 
         
 
@@ -119,7 +146,7 @@ class TableApplication:
         filemenu.add_command(label="New File", command=NewFile)
         filemenu.add_command(label="New Directory", command=NewDir)
         filemenu.add_command(label="Open File", command=OpenFile)
-        filemenu.add_command(label="Open Directory", command='''OpenNewFile''')
+        filemenu.add_command(label="Open Directory", command=OpenDirLoaded)
         filemenu.add_command(label="Save File", command='''SaveFile''')
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.app.quit)
@@ -135,13 +162,25 @@ class TableApplication:
         # filemenu.add_command(label="New Directory", command='''NewFile''')
         # To do
         # filemenu.add_command(label="Open Directory", command='''OpenNewFile''')
+    def InitFile(self):
+        ap.file = self.filepath
+        # print(ap.file)
+        ClearTable()
+
+        DataRef = ReadCSV(path=ap.file)
+        # print(DataRef)
+        data1 = []
+        for ls in DataRef:
+            data1.append(list(ls.values()))
+            # print(list(ls.values()))
+        length = len(list(data1)[0])
+        for contact in list(data1):
+            ap.treeTable.insert('', END, values=contact)
+
+        
     def InitDir(self):
-        # filedialog.askopenfilenames(initialdir=self.workdir)
-        # print((self.workdir))
-        # self.files = [os.path.join(self.workdir, item) for item in os.listdir(self.workdir)]
-        # for i in self.files:
-        #     print(i)
-        # print(self.workdir)
+        
+
         files: list[str] = []
         for i in list_folders_files(self.workdir):
             temp = i.replace(os.path.abspath("..\\")+"\\", "")
@@ -153,69 +192,71 @@ class TableApplication:
                 filesArr.append(i.split("/"))
         
         
-        print(filesArr)
-        self.treeFile.insert(parent="",index='end', text=filesArr[0][0], values='', iid=0)
-        self.treeFile.insert(parent=0,index='end', text=filesArr[0][1], values='', iid=1)
-        self.treeFile.insert(parent=1,index='end', text=filesArr[0][2], values='', iid=2)
-        self.treeFile.insert(parent=1,index='end', text=filesArr[1][2], values='', iid=3)
-        # self.treeFile.insert(parent=0,index='end', text="", values='----'+filesArr[1][1], iid=2)
-        # self.treeFile.insert(parent=0,index='end', text="", values='----'+filesArr[2][1], iid=3)
-        # self.treeFile.insert(parent=3,index='end', text="", values='--------'+filesArr[2][2], iid=0.1)
-        
-        # for file in filesArr:
-            
-        #     for i in range(len(file)):
-        #         self.treeFile.insert(file[0],END, values=file[i])
-
-        # self.treeFile.insert('', END,values='fil', iid='5')
-        # self.treeFile.insert('fil', END,values='filsc')
-        # self.treeFile.insert('5', END, values='data')
-        # self.treeFile.insert('5', END, values='data')
-        # self.treeFile.insert('data', END, values='cy.mouk')
+        # print(filesArr)
+        # self.treeFile.insert(parent="",index='end', text=filesArr[0][0], values='', iid=0)
+        # self.treeFile.insert(parent=0,index='end', text=filesArr[0][1], values='', iid=1)
+        # self.treeFile.insert(parent=1,index='end', text=filesArr[0][2], values='', iid=2)
+        # self.treeFile.insert(parent=1,index='end', text=filesArr[1][2], values='', iid=3)
 
     def FileManager(self, columns: tuple[str] = ('#0')):
-        # self.frameFile = LabelFrame(self.mainFrame, text= "Files", width = 150)
-        self.treeFile = Treeview(self.mainFrame)
-        # self.treeFile['columns'] = columns
+        self.frameFile = Frame(self.mainFrame, width = 150)
+        self.treeFile = Treeview(self.frameFile, selectmode="browse")
+        self.treeFile.bind("<<TreeviewSelect>>", self.get_selected_item_File)
+
         self.treeFile.heading(column='#0', text='File',anchor="center")
-        # self.treeFile.heading(column='#0', text='',anchor="center")
-
-        # self.treeFile.column('#0', stretch = True, width = 120, minwidth=25)
-        # self.treeFile.column('', width = 200, stretch = True)
         
-        scrollbarV = Scrollbar(self.mainFrame, orient=VERTICAL, command=self.treeFile.yview)
+        scrollbarV = Scrollbar(self.frameFile, orient=VERTICAL, command=self.treeFile.yview)
         self.treeFile.configure(yscroll=scrollbarV.set)
-        scrollbarV.grid(row=0, column=1, sticky='ns')
+        scrollbarV.pack(fill="both", side=RIGHT)
         
-        scrollbarH = Scrollbar(self.mainFrame, orient=HORIZONTAL, command=self.treeFile.xview)
+        scrollbarH = Scrollbar(self.frameFile, orient=HORIZONTAL, command=self.treeFile.xview)
         self.treeFile.configure(xscroll=scrollbarH.set)
-        scrollbarH.grid(row=1, column=0, sticky='ew')
+        scrollbarH.pack(fill="both", side=BOTTOM)
 
-
-        # self.treeFile.pack(fill="both", padx=5, pady=5)
-        self.treeFile.grid(row=0, column=0, sticky=NSEW)
+        self.treeFile.pack(fill="both", padx=5, pady=5, expand=1)
+        self.frameFile.grid(row=0, column=0, sticky=NSEW)
         self.InitDir()
-        # self.frameFile.pack(fill="both", side='left', padx=5, pady=5)
+        OpenDir()
+        self.InitFile()
+        
+    def get_selected_item_Data(self, event):        
+        selected_items = self.treeTable.selection()  # Get the IDs of selected items
+        for item in selected_items:
+            item_text = self.treeTable.item(item, "value")  # Get the value of each selected item
+            print("Selected item:", item_text)
+        
+    def get_selected_item_File(self, event):        
+        selected_items = self.treeFile.selection()  # Get the IDs of selected items
+        for item in selected_items:
+            item_text = self.treeFile.item(item, "value")[0]  # Get the value of each selected item
+            children = self.treeFile.get_children(item)
+            if len(children) == 0:
+                if(str(item_text).endswith(".csv")):
+                    print("Openning file:", item_text)
+                    OpenFile(item_text)
+                else:
+                    print("Can't open file:", item_text)
+
 
     def InitTable(self, columns: tuple[str] = ('id', 'name', 'quantity', 'price')):
-        # self.frame = LabelFrame(self.mainFrame, text= "Data", background="#000", foreground="#fff", border="1")
-        
-        # self.frametable = LabelFrame(self.mainFrame, text= "Data")
-        # self.frametable.bind("<Configure>", self.on_window_resize)
-       
-        self.treeTable = Treeview(self.mainFrame,columns=columns, show="headings", selectmode='extended')
+        self.frametable = Frame(self.mainFrame, width = 150)
+        self.treeTable = Treeview(self.frametable,columns=columns, show="headings", selectmode='extended')
+        self.treeTable.bind("<<TreeviewSelect>>", self.get_selected_item_Data)
         for val in columns:
             newVal = val.replace('_',' ').upper()
             self.treeTable.heading(column=val, text=newVal,anchor=CENTER)
-        # self.treeTable.column('id', width = 80, stretch = True, anchor="center")
-        # self.treeTable.column('name', stretch = True, anchor="center")
-        # self.treeTable.column('quantity', width = 120, stretch = True, anchor="center")
-        # self.treeTable.column('price', width = 120, stretch = True, anchor="center")
-        # self.treeTable.pack(fill="both", padx=5, pady=5)
-        self.treeTable.grid(row=0, column=2, sticky=NSEW)
-        # self.frametable.pack(fill="both", side="right", padx=5, pady=5)
-        # (row=0, column=0, padx=5, pady=5, ipadx=50, ipady=50)
-        # self.tableWidth = IntVar()
+            
+        scrollbarV = Scrollbar(self.frametable, orient=VERTICAL, command=self.treeTable.yview)
+        self.treeTable.configure(yscroll=scrollbarV.set)
+        scrollbarV.pack(fill="y", side=RIGHT)
+        
+        scrollbarH = Scrollbar(self.frametable, orient=HORIZONTAL, command=self.treeTable.xview)
+        self.treeTable.configure(xscroll=scrollbarH.set)
+        scrollbarH.pack(fill="x", side=BOTTOM)
+
+        self.treeTable.pack(fill="both", padx=5, pady=5, expand=1)
+        self.frametable.grid(row=0, column=1, sticky=NSEW)
+    
 
     def Update(self):
         self.app.mainloop()
@@ -225,17 +266,17 @@ columns: tuple[str] = ('id', 'medicament_Name', 'quantity', 'price')
 
 if __name__ == '__main__':
     ap = TableApplication("Hands Of Mine", 895,600)
-    # ap.InitTable()
+    ap.InitTable()
     ap.FileManager()
     s= Style()
 
-    s.theme_use('default')
-    s.configure("Treeview",
-        background="#D3D3D3",
-        foreground="black",
-        rowheight=25,
-        fieldbackground="#D3D3D3"       
-    )
+    s.theme_use(s.theme_names()[6])
+    # s.configure("Treeview",
+    #     background="#D3D3D3",
+    #     foreground="black",
+    #     rowheight=25,
+    #     fieldbackground="#D3D3D3"       
+    # )
     s.map("Treeview",
         background=[('selected', '#347083')])
     ap.Update()
--- Workdir/testBed.py
+++ Workdir/testBed.py
@@ -1,14 +1,101 @@
+import tkinter as tk
+from tkinter import ttk
+
+def get_selection(event=None):
+    selected_items = tree.selection()  # Get the IDs of selected items
+    for item in selected_items:
+        item_text = tree.item(item, "text")  # Get the text of each selected item
+        print("Selected item:", item_text)
+
+# Create the main window
+root = tk.Tk()
+root.title("TreeView Example")
+
+# Create a Treeview widget
+tree = ttk.Treeview(root)
+tree.pack()
+
+# Insert some items into the Treeview
+tree.insert("", "end", text="Item 1")
+tree.insert("", "end", text="Item 2")
+tree.insert("", "end", text="Item 3")
+
+# Bind the TreeviewSelect event to the get_selection function
+tree.bind("<<TreeviewSelect>>", get_selection)
+
+root.mainloop()
 
 
 
-from tkinter import Tk
-from tkinter.ttk import Treeview
-
-
-app = Tk()
-
-table = Treeview(master=app, show='headings')
 
 
 
-app.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# import tkinter as tk
+# from tkinter import ttk
+
+# def populate_tree(tree, node, path):
+#     """
+#     Recursively populate the Treeview widget with directories and subdirectories.
+
+#     Args:
+#     - tree: Treeview widget to populate.
+#     - node: Parent node ID.
+#     - path: Directory path.
+#     """
+#     # Iterate over directories in the current path
+#     for dir_name in os.listdir(path):
+#         print(dir_name)
+#         dir_path = os.path.join(path, dir_name)
+#         # Add directory to the Treeview
+#         node_id = tree.insert(node, "end", text=dir_name, open=False)
+#         # If the current item is a directory, recursively populate its subdirectories
+#         if os.path.isdir(dir_path):
+#             populate_tree(tree, node_id, dir_path)
+  
+            
+            
+
+# # Create the main window
+# root = tk.Tk()
+# root.title("Directory TreeView Example")
+
+# # Create a Treeview widget
+# tree = ttk.Treeview(root)
+# tree.pack(expand=True, fill=tk.BOTH)
+
+# # Get the list of directories (replace 'your_directory_path' with the actual directory path)
+# directory_path = os.path.abspath('')
+# # Ensure the directory exists
+# if os.path.exists(directory_path) and os.path.isdir(directory_path):
+#     # Populate the Treeview with directories and subdirectories
+#     populate_tree(tree, "", directory_path)
+# else:
+#     print("Directory does not exist.")
+
+# root.mainloop()
